cd72ce10a76e53fd4bbc7d28b83a0233a2e3e80f
amanguptaBay <47506369+amanguptaBay@users.noreply.github.com>
2020-10-02 15:48:29 -0700

    Initialized, single output bit

--- main.py
+++ main.py
@@ -3,16 +3,28 @@ import core
 
 def get_eqn_from_table(csv_file):
     tbl = pandas.read_csv(csv_file, dtype="str")
-    minterm_unreduced = core.get_minterms(tbl)
-    return core.simplify_minterms(minterm_unreduced, tbl)
+    return core.equationForSingleVariableTable(tbl)
+    
 def eqn_to_string(eqn):
+    vars = ["state","in"]
+    dims = len(eqn[0][0]),len(eqn[0][1])
+    def varTermsToString(termsForVar,varName):
+        out = ""
+        for idx,term in enumerate(termsForVar):
+            if term == "1":
+                out += f"{varName}{idx}"
+            elif term == "0":
+                out += f"{varName}{idx}'"
+        return out
+    eqnTerm_to_string = lambda term: varTermsToString(term[0],vars[0]) + varTermsToString(term[1],vars[1])
     arr_mterms = []
-    arr_mterms = map(lambda i: str(i),eqn)
-    return ("+ \n".join(arr_mterms))
+    arr_mterms = map(lambda i: eqnTerm_to_string(i),eqn)
+    return (" + ".join(arr_mterms))
 
 if __name__ == "__main__":
-    print("Provide CSV files to get a equation")
+    print("Provide CSV file to get a equation")
     file_csv = input("CSV File Path: ")
     eqn = get_eqn_from_table(file_csv)
     print("Minimized Solution:")
     print(eqn_to_string(eqn))
+
